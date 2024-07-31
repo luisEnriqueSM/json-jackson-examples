03a4e1829022a3c367c6bb2395cc74ddf9235665
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2024-07-31 11:06:56 -0600

    Initial commit

---
+++ src/main/java/org/json/jackson/examples/JsonJacksonExamplesApplication.java
@@ -0,0 +1,13 @@
+package org.json.jackson.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonJacksonExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonJacksonExamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/json/jackson/examples/JsonJacksonExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.json.jackson.examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonJacksonExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
